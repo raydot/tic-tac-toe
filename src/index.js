96a6523993acb4a32d3dda219fbb61ca35061e52
Dave Kanter <moatgoat@hotmail.com>
2021-07-01 16:42:50 -0700

    Start of 'Function Components' section

---
+++ src/index.js
@@ -0,0 +1,96 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+// Renders a single button
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+  render() {
+    return (
+      <button
+        className="square"
+        // onClick={function () { // replace with arrow function, of course
+        //   alert("click");
+        // }}
+        onClick={() => this.setState({ value: "X" })}
+      >
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+// Renders 9 squares
+class Board extends React.Component {
+  constructor(props) {
+    // let's manage state from the board
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const status = "Next: player: X";
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+// Board with placeholder values
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Game />, document.getElementById("root"));
